d8a8a8c4ea4c52a92ea0b7c45a508780b021f22d
AnthonyNjihia <antoh.njihia@gmail.com>
2022-09-12 12:25:17 -0700

    my 4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
-#!/bin/bash
-nclude <stdio.h>/**
+#include <stdio.h>
+/**
+ * main - Entry point
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
-
+ * Return: always 0
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
